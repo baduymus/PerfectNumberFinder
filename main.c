4f9e983bf6d321f087f2da9b2e0508fb1d05c2df
batu-han <arslanbatu97@gmail.com>
2022-12-04 16:23:22 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,34 @@
+/*
+ * @file		PerfectNumberFinder.c
+ * @Author: 	Batuhan ARSLAN
+ * @Purpose: 	Algorithm finds the perfect numbers. 
+ * @Date:		04/12/2022
+ */
+/* Includes *******************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+	int n = 0, sum = 0, i;
+	printf("Enter a number : ");
+	scanf("%d", &n);
+
+	for (i = 1; i < n; i++)
+	{
+		if (n % i == 0)
+		{
+			sum = sum + i;
+		}
+	}
+	printf("Sum  = %d \n", sum);
+	if (sum == n)
+	{
+		printf("It's a Perfect Number.");
+	}
+	else {
+		printf("It is not a Perfect Number.");
+	}
+	
+	return 0;
+}
